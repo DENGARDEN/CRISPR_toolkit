926cea36b5dc2fe739ab49f084fa5d8fee2fb81f
DENGARDEN <poowooho3@g.skku.edu>
2023-07-04 22:03:48 +0900

    User file now uses csv format

--- Indel_searcher_2/Run_cmd.sh
+++ Indel_searcher_2/Run_cmd.sh
@@ -4,23 +4,23 @@
 ## User parameter ##
 ###################################
 
-user=JaeWoo
-project=JaeWoo_test_samples
-pam_type=Cas9
+user=HDB
+project=TnpBPE
+pam_type=Cpf1
 pam_pos=Forward
-thread=15
+thread=16
 
 gap_open=-10 ## default
 gap_extend=1 ## default
 
 ###################################
 # TODO: if there is space characters...
-while read python_path;do
+while read python_path; do
     python=$python_path
-done < ../PythonPath.txt
+done <../PythonPath.txt
 
-[ ! -d ./Output/${user} ] && eval mkdir ./Output/${user};
-[ ! -d ./Output/${user}/${project} ] && eval mkdir ./Output/${user}/${project};
-[ ! -d ./Output/${user}/${project}/Log ] && eval mkdir ./Output/${user}/${project}/Log;
+[ ! -d ./Output/${user} ] && eval mkdir ./Output/${user}
+[ ! -d ./Output/${user}/${project} ] && eval mkdir ./Output/${user}/${project}
+[ ! -d ./Output/${user}/${project}/Log ] && eval mkdir ./Output/${user}/${project}/Log
 
-nohup $python ./Run_indel_searcher.py --python $python --user $user --project $project --pam_type $pam_type --pam_pos $pam_pos -t $thread > ./Output/${user}/${project}/Log/log.txt 2>&1
+nohup $python ./Run_indel_searcher.py --python $python --user $user --project $project --pam_type $pam_type --pam_pos $pam_pos -t $thread >./Output/${user}/${project}/Log/log.txt 2>&1
